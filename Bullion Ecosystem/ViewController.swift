827a3e5099711f6e7d8efe880ed22251805eaa4d
Devin <devin@cyberindo-sinergi.com>
2024-06-21 14:57:25 +0700

    Initial Commit

---
+++ Bullion Ecosystem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bullion Ecosystem
+//
+//  Created by Cyberindo Sinergi Sistem on 21/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
